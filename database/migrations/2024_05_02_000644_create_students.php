81e712df0a3e9098d5cb8877cd08f215e7b5e24b
fabian emiliano rayon sotelo <emiliano8907@gmail.com>
2024-05-03 12:37:28 -0600

    install dependencies

---
+++ database/migrations/2024_05_02_000644_create_students.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',10);
+            $table->string('last name');
+            $table->string('mother last name');
+            $table->string('curp')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
